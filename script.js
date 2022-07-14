c75aaf95a581b0e8de45071c5e0a3d930dc97472
Michael <edu.michaelstetter@gmail.com>
2022-07-13 23:18:04 -0400

    Created function that generates the computers move/decision

---
+++ script.js
@@ -0,0 +1,15 @@
+const computerPlay = () => {
+    let computerChoice = null;
+    switch (Math.floor(Math.random() * 3) + 1){
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+    }
+    return computerChoice;
+}
